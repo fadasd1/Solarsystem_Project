92a66b63dd040d73c28e4f0a8e485c64b7abe815
fadasd <irfanoezkan.65@gmail.com>
2025-12-24 21:26:50 +0100

    German -> English

--- src/method_classes/VectorMathImpl.java
+++ src/method_classes/VectorMathImpl.java
@@ -21,11 +21,11 @@ public class VectorMathImpl implements VectorMath {
 		return new Vec(v1.getX() - v2.getX(), v1.getY() - v2.getY(), v1.getZ() - v2.getZ());
 	}
 
-	private double betrag(Vector v1) {
+	private double magnitude(Vector v1) {
 		return Math.hypot(v1.getX(), Math.hypot(v1.getY(), v1.getZ()));
 	}
 
-	private double skalarProduktKoordinaten(Vector v1, Vector v2)  {
+	private double dotProductCoordinates(Vector v1, Vector v2)  {
 		double max1 = Math.max(Math.abs(v1.getX()), Math.abs(v1.getY()));
 		max1 = Math.max(max1, Math.abs(v1.getZ()));
 
@@ -36,11 +36,11 @@ public class VectorMathImpl implements VectorMath {
 		double x1 = v1.getX() / max1, y1 = v1.getY() / max1, z1 = v1.getZ() / max1;
 		double x2 = v2.getX() / max2, y2 = v2.getY() / max2, z2 = v2.getZ() / max2;
 
-		double produkt =  x1*x2 + y1*y2 + z1*z2;
-		return produkt * max1 * max2;
+		double product =  x1*x2 + y1*y2 + z1*z2;
+		return product * max1 * max2;
 	}
 
-	/*private double skalarProduktWinkel(Vector v1, Vector v2, double) {
+	/*private double dotProductAngle(Vector v1, Vector v2, double angle) {
 		double winkel =
 	}*/
 
